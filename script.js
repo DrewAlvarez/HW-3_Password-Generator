13384d9cd00c66a32cad79e082e6e41eb2f4b305
Andrew <alvarez.drew@gmail.com>
2020-08-07 13:02:18 -0500

    added alerts for minimum requirements to password criteria.

--- script.js
+++ script.js
@@ -5,19 +5,24 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
 
-  var pwLength = ""
-  while (pwLength<8 || pwLength>128){
-    pwLength = prompt("Choose any length (8-128) for your password.");
+  var pwLength = prompt("Choose any length (8-128) for your password.");
+  
+//confirm length of pw is 8-128
+  if (pwLength < 8 || pwLength > 128){
+    alert("Your must be between 8-128 long, please try again");
+    return writePassword;
   }
   
-  console.log (pwLength)
 
-  var pwLowercase = confirm("Do you want to include lowercase?")
-  console.log(pwLowercase)
-  var pwUppercase = confirm("Do you want to include uppercase?")
-  var pwNumbers = confirm("Do you want to include numbers?")
-  var pwSpecialChar = confirm("Do you want to include special characters? \n(!, @, #, $ .etc)")
-
+  var pwLowercase = confirm("Do you want to include lowercase?");
+  var pwUppercase = confirm("Do you want to include uppercase?");
+  var pwNumbers = confirm("Do you want to include numbers?");
+  var pwSpecialChar = confirm("Do you want to include special characters? \n(!, @, #, $ .etc)");
+  //conirm that at least one is true
+  if(pwLowercase == false && pwUppercase == false && pwNumbers == false && pwSpecialChar == false){
+    alert("You must choose at least one criteria for you password! Please try again!")
+    return writePassword;
+  }
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -44,72 +49,72 @@ function writePassword() {
     for (var i = 1; i <= pwLength; i++){
       //for lowercase only
       if(pwLowercase==true && pwUppercase!==true && pwNumbers!==true && pwSpecialChar!==true){
-        password += lowerCaseChar.charAt(Math.floor(Math.random()*lowerCaseChar.length + 1))
+        password += lowerCaseChar.charAt(Math.floor(Math.random()*lowerCaseChar.length + 1));
 
       }
       //for uppercase only
       else if((pwLowercase!==true && pwUppercase==true && pwNumbers!==true && pwSpecialChar!==true)){
-        password += upperCaseChar.charAt(Math.floor(Math.random()*upperCaseChar.length + 1))
+        password += upperCaseChar.charAt(Math.floor(Math.random()*upperCaseChar.length + 1));
 
       }
       //for numeric only
       else if((pwLowercase!==true && pwUppercase==!true && pwNumbers==true && pwSpecialChar!==true)){
-        password += numericChar.charAt(Math.floor(Math.random()*numericChar.length + 1))
+        password += numericChar.charAt(Math.floor(Math.random()*numericChar.length + 1));
 
       }
       //for special characters only
       else if((pwLowercase!==true && pwUppercase==!true && pwNumbers!==true && pwSpecialChar==true)){
-        password += specialChar.charAt(Math.floor(Math.random()*specialChar.length + 1))
+        password += specialChar.charAt(Math.floor(Math.random()*specialChar.length + 1));
 
       }
       //for lower and upper case only
       else if((pwLowercase==true && pwUppercase==true && pwNumbers!==true && pwSpecialChar!==true)){
-        password += lowerUpper.charAt(Math.floor(Math.random()*lowerUpper.length + 1))
+        password += lowerUpper.charAt(Math.floor(Math.random()*lowerUpper.length + 1));
 
       }
       //for lowercase and numeric only
       else if((pwLowercase==true && pwUppercase!==true && pwNumbers==true && pwSpecialChar!==true)){
-        password += lowerNum.charAt(Math.floor(Math.random()*lowerNum.length + 1))
+        password += lowerNum.charAt(Math.floor(Math.random()*lowerNum.length + 1));
 
       }
       //for lowercase and special characters only
       else if((pwLowercase==true && pwUppercase==!true && pwNumbers!==true && pwSpecialChar==true)){
-        password += lowerSpecial.charAt(Math.floor(Math.random()*lowerSpecial.length + 1))
+        password += lowerSpecial.charAt(Math.floor(Math.random()*lowerSpecial.length + 1));
 
       }
       //for uppercase and numeric only
       else if((pwLowercase!==true && pwUppercase==true && pwNumbers==true && pwSpecialChar!==true)){
-        password += upperNum.charAt(Math.floor(Math.random()*upperNum.length + 1))
+        password += upperNum.charAt(Math.floor(Math.random()*upperNum.length + 1));
 
       }
       //for uppercase and special characters only
       else if((pwLowercase!==true && pwUppercase==true && pwNumbers!==true && pwSpecialChar== true)){
-        password += upperSpecial.charAt(Math.floor(Math.random()*upperSpecial.length + 1))
+        password += upperSpecial.charAt(Math.floor(Math.random()*upperSpecial.length + 1));
 
       }
       //for numeric and special characters only
       else if((pwLowercase!==true && pwUppercase==!true && pwNumbers==true && pwSpecialChar==true)){
-        password += numSpecial.charAt(Math.floor(Math.random()*numSpecial.length + 1))
+        password += numSpecial.charAt(Math.floor(Math.random()*numSpecial.length + 1));
 
       }
       //for lowercase, uppercase and numeric only
       else if((pwLowercase==true && pwUppercase==true && pwNumbers==true && pwSpecialChar!==true)){
-        password += lowerUpperNum.charAt(Math.floor(Math.random()*lowerUpperNum.length + 1))
+        password += lowerUpperNum.charAt(Math.floor(Math.random()*lowerUpperNum.length + 1));
 
       }
       //for lowercase, uppercase and special characters only
       else if((pwLowercase==true && pwUppercase==true && pwNumbers!==true && pwSpecialChar==true)){
-        password += lowerUpperSpecial.charAt(Math.floor(Math.random()*lowerUpperSpecial.length + 1))
+        password += lowerUpperSpecial.charAt(Math.floor(Math.random()*lowerUpperSpecial.length + 1));
 
       }
       //for uppercase, numeric and special characters only
       else if((pwLowercase!==true && pwUppercase==true && pwNumbers==true && pwSpecialChar==true)){
-        password += upperNumSpecial.charAt(Math.floor(Math.random()*upperNumSpecial.length + 1))
+        password += upperNumSpecial.charAt(Math.floor(Math.random()*upperNumSpecial.length + 1));
 
       }
       //for all characters
       else if((pwLowercase==true && pwUppercase==true && pwNumbers==true && pwSpecialChar==true)){
-        password += allChar.charAt(Math.floor(Math.random()*allChar.length + 1))
+        password += allChar.charAt(Math.floor(Math.random()*allChar.length + 1));
 
       }
 
